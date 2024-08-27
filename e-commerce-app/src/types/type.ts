4cf5121074fbfb7d828d56f5e754440e529bdb07
Arya Wibowo Jati <aryajati29@gmail.com>
2024-08-27 12:21:53 +0700

    feat: add initial type interfaces

---
+++ e-commerce-app/src/types/type.ts
@@ -0,0 +1,34 @@
+export interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+export interface Product {
+  _id: string;
+  name: string;
+  slug: string;
+  description: string;
+  excerpt: string;
+  price: number;
+  tags: string[];
+  thumbnail: string;
+  images: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Wishlist {
+  _id: string;
+  userId: string;
+  productId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type Users = User[];
+
+export type Products = Product[];
+
+export type Wishlists = Wishlist[];
