28e9cd74899f05f0a429f1c409ed45e8f6058757
Arya Wibowo Jati <aryajati29@gmail.com>
2024-08-27 14:46:16 +0700

    feat: add initial setup for mongodb

---
+++ e-commerce-app/src/db/config/index.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+if (!connectionString) {
+  throw new Error("MONGODB_CONNECTION_STRING is not defined");
+}
+
+let client: MongoClient;
+export const getDBInstance = async () => {
+  if (!client) {
+    client = new MongoClient(connectionString);
+    await client.connect();
+  }
+  return client;
+};
