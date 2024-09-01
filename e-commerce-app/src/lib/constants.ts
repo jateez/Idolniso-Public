0e35c118a2982bec3b1ddd56fc69dc86f791f94f
Arya Wibowo Jati <aryajati29@gmail.com>
2024-09-01 21:55:34 +0700

    fix: env for deployment

--- e-commerce-app/src/lib/constants.ts
+++ e-commerce-app/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL: string = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
+export const API_BASE_URL: string = `${process.env.NEXT_PUBLIC_BASE_URL}/api` || "http://localhost:3000/api";
 export const COLLECTION_USER: string = "users";
 export const COLLECTION_PRODUCT: string = "products";
 export const COLLECTION_WISHLIST: string = "wishlists";
